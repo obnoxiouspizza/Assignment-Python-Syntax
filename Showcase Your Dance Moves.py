c846567d1248d5329f67fe709d72c0305e5ec017
obnoxiouspizza <david.diaz2000@icloud.com>
2024-12-10 21:37:14 -0600

    Completed Question 1

---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,21 @@
+# Question 1 Task 1
+
+# Current weather
+weather = "sunny"
+
+# Determining what to bring
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Question 1 Task 2
+
+# Ask user how they are feeling
+mood = input("How are you feeling today?")
+
+#Check mood and reply
+if mood == "happy":
+    print("That's great to hear!")
+elif mood == "sad":
+    print("I hope your day gets better!")
